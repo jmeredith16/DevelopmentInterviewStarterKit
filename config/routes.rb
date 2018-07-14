e26f49189bf0d7ff67c7cc676bc6d204921ee4da
Joe Meredith <17982571+jmeredith16@users.noreply.github.com>
2018-07-14 17:08:29 -0400

    Add new people route and link to it

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  namespace :api, defaults: { format: "json" } do
-    get :me, to: 'me#me'
-  end
 
   root to: "main#index"
+  resources :people
 end
